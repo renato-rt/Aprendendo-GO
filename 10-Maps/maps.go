5cb38dd7946f6b4a8b350dcb184e79e49848c957
Renato Tomikawa <tomikawa.renato@gmail.com>
2021-08-09 10:05:39 -0300

    07-08

--- 10-Maps/maps.go
+++ 10-Maps/maps.go
@@ -4,10 +4,16 @@ import "fmt"
 
 func main() {
 	fmt.Println("Maps")
-
+//Map só aceita que seja um tipo todos os campos
 	usuario := map[string]string {
 		"nome":			"Pedro",
 		"sobrenome":	"Silva",
 	}
-	fmt.Println(usuario)
+	fmt.Println(usuario["nome"])
+
+	usuario2 := map[string]map[string]string(
+		"nome:": {
+				"primeiro": "João"
+		}
+	)
 }
